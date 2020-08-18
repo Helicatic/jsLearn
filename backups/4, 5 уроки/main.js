0d65b8d76efe1ecb5f6b6ea28e406f0077b4d655
Rustam <rustammagic9@gmail.com>
2020-08-18 22:29:15 +0500

    first commit

---
+++ backups/4, 5 уроки/main.js
@@ -0,0 +1,19 @@
+"use strict";
+
+a = 15;
+console.log(a);
+
+let bufer = {
+  name: "Islom",
+  age: 17,
+  bufferingLevel: 100000000,
+  isBuferBoss: true
+}
+
+console.log(bufer.bufferingLevel)
+
+{
+  let raz = 1;
+}
+
+console.log(raz);
---
+++ main.js
@@ -0,0 +1,9 @@
+"use strict";
+
+const category = "toys";
+
+console.log(`https://github.com/${category}/5`);
+
+const username = "Rustam";
+
+alert(`Hello, ${username}`);
