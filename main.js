b797f5ce76758157eaefeabd2fd836232353cac6
Helicatic <arauzworker@gmail.com>
2020-08-20 00:35:22 +0500

    Урок 12 работа с объектами и prompt

--- main.js
+++ main.js
@@ -1,9 +1,35 @@
-"use strict";
+// "use strict";
 
-const category = "toys";
 
-console.log(`https://github.com/${category}/5`);
+const numberOfFilms = +prompt("Сколько фильмов уже посмотрели", "");
 
-const username = "Rustam";
+// console.log(numberOfFilms);
 
-alert(`Hello, ${username}`);
+let personalMoviesDB = {
+  count: numberOfFilms,
+  movies: {},
+  actors: {},
+  genres: [],
+  privat: false,
+};
+
+
+const latestFilm = prompt("Один из последних просмотренных фильмов?", ""),
+  filmRate = prompt("На сколько оцените его?", ""),
+  latestFilm_2 = prompt("Один из последних просмотренных фильмов?", ""),
+  filmRate_2 = prompt("На сколько оцените его?", "");
+
+let movies = {
+  movies: {
+
+  }
+}
+
+
+personalMoviesDB.movies[latestFilm] = filmRate;
+personalMoviesDB.movies[latestFilm_2] = filmRate_2;
+
+
+
+console.log(personalMoviesDB);
+console.log(personalMoviesDB.movies['latestFilm']);
