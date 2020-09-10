f0ca27e7c153252dd0126b180b52232ba1dda854
Helicatic <arauzworker@gmail.com>
2020-09-10 15:36:06 +0500

    Урок 18 Пркатика с функциями

--- main.js
+++ main.js
@@ -1,19 +1,82 @@
 "use strict";
 
-const person = "Azimov Rustam Asqar o'gli";
-const college = "Tashkent Pro IT College";
-const age = 18.8;
-const length = "182.6cm";
-const mass = "62.3kg";
+/* Задание на урок:
 
-const personName = person.slice(7, 13);
-const personSurname = person.slice(0, 6);
-const personFatherName = person.substring(14);
+1) Первую часть задания повторить по уроку
 
-console.log(personName);
-console.log(personSurname);
-console.log(personFatherName);
-console.log(college.substr(0, 8));
-console.log(Math.round(age));
-console.log(parseInt(length));
-console.log(parseFloat(mass));
+2) Создать функцию showMyDB, которая будет проверять свойство privat. Если стоит в позиции
+false - выводит в консоль главный объект программы
+
+3) Создать функцию writeYourGenres в которой пользователь будет 3 раза отвечать на вопрос 
+"Ваш любимый жанр под номером ${номер по порядку}". Каждый ответ записывается в массив данных
+genres
+
+P.S. Функции вызывать не обязательно*/
+
+
+
+let numberOfFilms;
+
+function start() {
+  numberOfFilms = +prompt('Сколько фильмов вы уже посмотрели?', '');
+
+  while (numberOfFilms == '' || numberOfFilms == null || isNaN(numberOfFilms)) {
+    numberOfFilms = +prompt('Сколько фильмов вы уже посмотрели?', '');
+  }
+}
+
+start();
+
+const personalMoviesDB = {
+  count: numberOfFilms,
+  movies: {},
+  actors: {},
+  genres: [],
+  privat: false,
+}
+
+function rememberMyFilms() {
+  for (let i = 0; i < 2; i++) {
+    const a = prompt('Один из просмотренных фильмов?', ''),
+      b = prompt('На сколько оцените его?', '');
+
+    if (a != null && b != null && a != '' && b != '' && a.length < 50) {
+      personalMoviesDB.movies[a] = b;
+      console.log('done');
+    } else {
+      console.log('error');
+      i--;
+    }
+  }
+}
+rememberMyFilms();
+
+function detectPersonalLevel() {
+  if (personalMoviesDB.count < 10) {
+    console.log('Просмотренно довольно мало фильмов');
+  } else if (personalMoviesDB.count >= 10 && personalMoviesDB.count < 30) {
+    console.log('Вы классический зритель');
+  } else if (personalMoviesDB.count >= 30) {
+    console.log('Вы киноман!');
+  } else {
+    console.log('Произошла ошибка');
+  }
+}
+
+detectPersonalLevel();
+
+function showMyDB(hidden) {
+  if (!hidden) {
+    console.log(personalMoviesDB);
+  }
+}
+
+showMyDB(personalMoviesDB.privat);
+
+function writeYourGenres() {
+  for (let i = 1; i <= 3; i++) {
+    personalMoviesDB.genres[i - 1] = prompt(`Ваш любимый жанр под номером ${i}?`, '');;
+  }
+}
+
+writeYourGenres();
