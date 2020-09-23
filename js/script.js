dd246a9144923b14cac9dacfa437b4567ffe154b
Helicatic <helicatic@users.noreply.github.com>
2020-09-23 17:21:37 +0500

    Урок 33 Практика. Используем события на странице проекта

--- js/script.js
+++ js/script.js
@@ -17,72 +17,87 @@ P.S. Здесь есть несколько вариантов решения з
 
 "use strict";
 
-const movieDB = {
-  movies: [
-    "Логан",
-    "Лига справедливости",
-    "Ла-ла лэнд",
-    "Одержимость",
-    "Скотт Пилигрим против...",
-    "Аполлон 13",
-  ],
-};
+document.addEventListener("DOMContentLoaded", () => {
+  const movieDB = {
+    movies: [
+      "Логан",
+      "Лига справедливости",
+      "Ла-ла лэнд",
+      "Одержимость",
+      "Скотт Пилигрим против...",
+      "Аполлон 13",
+    ],
+  };
 
-const ads = document.querySelectorAll(".promo__adv img"),
-  poster = document.querySelector(".promo__bg"),
-  genre = poster.querySelector(".promo__genre"),
-  films = document.querySelectorAll(".promo__interactive-item"),
-  movieList = document.querySelector(".promo__interactive-list"),
-  filmInput = document.querySelector(".adding__input"),
-  filmAddBtn = document.querySelector(".add button"),
-  filmRemoveBtns = document.querySelectorAll(".delete");
+  const ads = document.querySelectorAll(".promo__adv img"),
+    poster = document.querySelector(".promo__bg"),
+    genre = poster.querySelector(".promo__genre"),
+    films = document.querySelectorAll(".promo__interactive-item"),
+    movieList = document.querySelector(".promo__interactive-list"),
+    addForm = document.querySelector("form.add"),
+    addInput = addForm.querySelector(".adding__input"),
+    checkbox = addForm.querySelector('[type="checkbox"]');
 
-ads.forEach((item) => {
-  item.remove();
-});
+  addForm.addEventListener("submit", (e) => {
+    e.preventDefault();
+    let newFilm = addInput.value;
+    const favorite = checkbox.checked;
 
-genre.textContent = "Драма";
+    if (newFilm) {
+      if (newFilm.length > 21) {
+        newFilm = `${newFilm.substring(0, 22)}...`;
+      }
 
-poster.style.backgroundImage = 'url("img/bg.jpg")';
+      if (favorite) {
+        console.log("Добавляем любимый фильм");
+      }
 
-movieList.innerHTML = "";
+      movieDB.movies.push(newFilm);
+      sortArr(movieDB.movies);
 
-movieDB.movies.forEach((film, i) => {
-  movieList.innerHTML += `
-        <li class="promo__interactive-item">${i + 1}: ${film}
-            <div class="delete"></div>
-        </li>
-    `;
-});
+      createMovieList(movieDB.movies, movieList);
+    }
 
-filmAddBtn.addEventListener("click", (e) => {
-  e.preventDefault();
-  let addingFilm = filmInput.value;
-  if (addingFilm.length > 21) {
-    addingFilm = `${addingFilm.slice(0, 21)}...`;
+    e.target.reset();
+  });
+
+  const deleteAds = (arr) => {
+    arr.forEach((item) => {
+      item.remove();
+    });
+  };
+
+  const makeChanges = () => {
+    genre.textContent = "Драма";
+    poster.style.backgroundImage = 'url("img/bg.jpg")';
+  };
+
+  const sortArr = (arr) => {
+    arr.sort();
+  };
+
+  function createMovieList(films, parent) {
+    parent.innerHTML = "";
+    sortArr(films);
+
+    films.forEach((film, i) => {
+      parent.innerHTML += `
+            <li class="promo__interactive-item">${i + 1}: ${film}
+                <div class="delete"></div>
+            </li>
+        `;
+    });
+
+    document.querySelectorAll(".delete").forEach((btn, i) => {
+      btn.addEventListener("click", () => {
+        btn.parentElement.remove();
+        movieDB.movies.splice(i, 1);
+        createMovieList(films, parent);
+      });
+    });
   }
-  if (addingFilm != "" && addingFilm != null) {
-    movieDB.movies.push(filmInput.value);
-    console.log(movieDB.movies);
-    movieDB.movies.sort();
-    movieList.innerHTML += `
-    <li class="promo__interactive-item">${movieDB.movies.length}: ${addingFilm}
-            <div class="delete"></div>
-    </li>
-    `;
-    filmInput.value = "";
-  }
+  deleteAds(ads);
+  makeChanges();
+  sortArr(movieDB.movies);
+  createMovieList(movieDB.movies, movieList);
 });
-console.log(filmRemoveBtns);
-// filmRemoveBtns.forEach((btn, i) => {
-//   btn.addEventListener("click", (e) => {
-//     e.preventDefault();
-//     console.log(`click on btn ${i}`);
-//     console.log(e.target);
-//   });
-// });
-
-// filmRemoveBtns[0].addEventListener("click", (e) => {
-//   e.preventDefault;
-//   console.log("Click");
-// });
