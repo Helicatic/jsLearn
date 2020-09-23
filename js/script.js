69d8f5aa960a80a08ed89ba2c5510d9e8b8a530b
Helicatic <helicatic@users.noreply.github.com>
2020-09-23 21:54:09 +0500

    Урок 34 События на мобильных устройствах

--- js/script.js
+++ js/script.js
@@ -1,103 +1,27 @@
-/* Задания на урок:
-
-1) Реализовать функционал, что после заполнения формы и нажатия кнопки "Подтвердить" - 
-новый фильм добавляется в список. Страница не должна перезагружаться.
-Новый фильм должен добавляться в movieDB.movies.
-Для получения доступа к значению input - обращаемся к нему как input.value;
-P.S. Здесь есть несколько вариантов решения задачи, принимается любой, но рабочий.
-
-2) Если название фильма больше, чем 21 символ - обрезать его и добавить три точки
-
-3) При клике на мусорную корзину - элемент будет удаляться из списка (сложно)
-
-4) Если в форме стоит галочка "Сделать любимым" - в консоль вывести сообщение: 
-"Добавляем любимый фильм"
-
-5) Фильмы должны быть отсортированы по алфавиту */
-
 "use strict";
 
-document.addEventListener("DOMContentLoaded", () => {
-  const movieDB = {
-    movies: [
-      "Логан",
-      "Лига справедливости",
-      "Ла-ла лэнд",
-      "Одержимость",
-      "Скотт Пилигрим против...",
-      "Аполлон 13",
-    ],
-  };
+window.addEventListener("DOMContentLoaded", () => {
+  const box = document.querySelector(".box");
 
-  const ads = document.querySelectorAll(".promo__adv img"),
-    poster = document.querySelector(".promo__bg"),
-    genre = poster.querySelector(".promo__genre"),
-    films = document.querySelectorAll(".promo__interactive-item"),
-    movieList = document.querySelector(".promo__interactive-list"),
-    addForm = document.querySelector("form.add"),
-    addInput = addForm.querySelector(".adding__input"),
-    checkbox = addForm.querySelector('[type="checkbox"]');
+  // box.addEventListener("touchstart", (e) => {
+  //   e.preventDefault();
 
-  addForm.addEventListener("submit", (e) => {
+  //   console.log("Touch");
+  //   console.log(e.touches);
+  //   console.log(e.targetTouches);
+  //   console.log(e.changedTouches);
+  // });
+
+  box.addEventListener("touchmove", (e) => {
     e.preventDefault();
-    let newFilm = addInput.value;
-    const favorite = checkbox.checked;
 
-    if (newFilm) {
-      if (newFilm.length > 21) {
-        newFilm = `${newFilm.substring(0, 22)}...`;
-      }
-
-      if (favorite) {
-        console.log("Добавляем любимый фильм");
-      }
-
-      movieDB.movies.push(newFilm);
-      sortArr(movieDB.movies);
-
-      createMovieList(movieDB.movies, movieList);
-    }
-
-    e.target.reset();
+    // console.log("Move");
+    console.log(e.targetTouches[0].pageX);
   });
 
-  const deleteAds = (arr) => {
-    arr.forEach((item) => {
-      item.remove();
-    });
-  };
+  // box.addEventListener("touchend", (e) => {
+  //   e.preventDefault();
 
-  const makeChanges = () => {
-    genre.textContent = "Драма";
-    poster.style.backgroundImage = 'url("img/bg.jpg")';
-  };
-
-  const sortArr = (arr) => {
-    arr.sort();
-  };
-
-  function createMovieList(films, parent) {
-    parent.innerHTML = "";
-    sortArr(films);
-
-    films.forEach((film, i) => {
-      parent.innerHTML += `
-            <li class="promo__interactive-item">${i + 1}: ${film}
-                <div class="delete"></div>
-            </li>
-        `;
-    });
-
-    document.querySelectorAll(".delete").forEach((btn, i) => {
-      btn.addEventListener("click", () => {
-        btn.parentElement.remove();
-        movieDB.movies.splice(i, 1);
-        createMovieList(films, parent);
-      });
-    });
-  }
-  deleteAds(ads);
-  makeChanges();
-  sortArr(movieDB.movies);
-  createMovieList(movieDB.movies, movieList);
+  //   console.log("Touch end");
+  // });
 });
