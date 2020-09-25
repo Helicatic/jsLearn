e773ef51eeea0de394d94fffee031cb5df6682f8
Helicatic <helicatic@users.noreply.github.com>
2020-09-25 13:31:49 +0500

    Урок 38 Создаем табы в новом проекте

--- js/script.js
+++ js/script.js
@@ -1,54 +1,40 @@
 "use strict";
 
-const btns = document.querySelectorAll('button'),
-  wrapper = document.querySelector('.button-block');
+document.addEventListener("DOMContentLoaded", () => {
+  const tabs = document.querySelectorAll(".tabheader__item"),
+    tabsContent = document.querySelectorAll(".tabcontent"),
+    tabsParent = document.querySelector(".tabheader__items");
 
-// console.log(btns[0].classList.length);
-// console.log(btns[0].classList.item(0));
-// console.log(btns[1].classList.add('red', 'durak'));
-// console.log(btns[0].classList.remove('blue'));
-// console.log(btns[0].classList.toggle('blue'));
+  function hideTabContent() {
+    tabsContent.forEach((item) => {
+      item.classList.add("hide");
+      item.classList.remove("show", "fade");
+    });
 
-// if (btns[1].classList.contains('red')) {
-//   console.log('red');
-// }
-
-btns[0].addEventListener('click', () => {
-  // if (!btns[1].classList.contains('red')) {
-  //   btns[1].classList.add('red');
-  // } else {
-  //   btns[1].classList.remove('red');
-  // }
-
-  btns[1].classList.toggle('red');
-});
-
-// console.log(btns[0].className);
-
-// wrapper.addEventListener('click', (e) => {
-//   if (e.target && e.target.classList.contains('blue')) {
-//     console.log('Hello');
-//   }
-// })
-// wrapper.addEventListener('click', (e) => {
-//   if (e.target && e.target.tagName == 'BUTTON') {
-//     console.log('Hello');
-//   }
-// })
-
-wrapper.addEventListener('click', (e) => {
-  if (e.target && e.target.matches("button.red")) {
-    console.log('Hello');
+    tabs.forEach((item) => {
+      item.classList.remove("tabheader__item_active");
+    });
   }
-})
 
-// btns.forEach(btn => {
-//   btn.addEventListener('click', () => {
-//     console.log('hello');
-//   })
-//   ''
-// });
+  function showTabContent(i = 0) {
+    tabsContent[i].classList.add("show", "fade");
+    tabsContent[i].classList.remove("hide");
+    tabs[i].classList.add("tabheader__item_active");
+  }
 
-const btn = document.createElement('button');
-btn.classList.add('red');
-wrapper.append(btn);
+  hideTabContent();
+  showTabContent();
+
+  tabsParent.addEventListener("click", (e) => {
+    const target = e.target;
+
+    if (target && target.classList.contains("tabheader__item")) {
+      tabs.forEach((item, i) => {
+        if (target == item) {
+          hideTabContent();
+          showTabContent(i);
+        }
+      });
+    }
+  });
+});
