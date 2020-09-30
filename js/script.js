7a27d2d90513b26048dc0bf5fc8459da1115167b
Helicatic <arauzworker@gmail.com>
2020-09-30 12:43:38 +0500

    Урок 45 Функции-конструкторы

--- js/script.js
+++ js/script.js
@@ -1,168 +1,28 @@
 "use strict";
-document.addEventListener("DOMContentLoaded", () => {
 
-  // TABS
+// const num = new Number(5);
+// console.log(num);
 
-  const tabs = document.querySelectorAll(".tabheader__item"),
-    tabsContent = document.querySelectorAll(".tabcontent"),
-    tabsParent = document.querySelector(".tabheader__items");
-
-  function hideTabContent() {
-    tabsContent.forEach((item) => {
-      item.classList.add("hide");
-      item.classList.remove("show", "fade");
-    });
-
-    tabs.forEach((item) => {
-      item.classList.remove("tabheader__item_active");
-    });
+function User(name, id) {
+  this.name = name;
+  this.id = id;
+  this.human = true;
+  this.hello = function() {
+    console.log(`Hello ${this.name}!`);
   }
+}
 
-  function showTabContent(i = 0) {
-    tabsContent[i].classList.add("show", "fade");
-    tabsContent[i].classList.remove("hide");
-    tabs[i].classList.add("tabheader__item_active");
-  }
+User.prototype.exit = function() {
+  console.log(`Пользователь ${this.name} покинул чат)`);
+}
 
-  hideTabContent();
-  showTabContent();
+const ivan = new User('Ivan', 30);
+const arauz = new User('Arauz', 18);
 
-  tabsParent.addEventListener("click", (e) => {
-    const target = e.target;
+ivan.exit();
 
-    if (target && target.classList.contains("tabheader__item")) {
-      tabs.forEach((item, i) => {
-        if (target == item) {
-          hideTabContent();
-          showTabContent(i);
-        }
-      });
-    }
-  });
+ivan.hello();
+arauz.hello();
 
-  // TIMER 
-
-  const deadline = '2020-10-12';
-
-  function getTimeRemaining(endtime) {
-    const t = Date.parse(endtime) - Date.parse(new Date()),
-      days = Math.floor(t / (1000 * 60 * 60 * 24)),
-      hours = Math.floor((t / (1000 * 60 * 60) % 24)),
-      minutes = Math.floor((t / 1000 / 60) % 60),
-      seconds = Math.floor((t / 1000) % 60);
-
-    return {
-      'total': t,
-      'days': days,
-      'hours': hours,
-      'minutes': minutes,
-      'seconds': seconds
-    };
-  }
-
-  function getZero(num) {
-    if (num >= 0 && num < 10) {
-      return `0${num}`;
-    } else {
-      return num;
-    }
-  }
-
-  function setClock(selector, endtime) {
-    const timer = document.querySelector(selector),
-      days = timer.querySelector('#days'),
-      hours = timer.querySelector('#hours'),
-      minutes = timer.querySelector('#minutes'),
-      seconds = timer.querySelector('#seconds'),
-      timeInterval = setInterval(updateClock, 1000);
-
-    updateClock();
-
-    function updateClock() {
-      const t = getTimeRemaining(endtime);
-
-      days.innerHTML = getZero(t.days);
-      hours.innerHTML = getZero(t.hours);
-      minutes.innerHTML = getZero(t.minutes);
-      seconds.innerHTML = getZero(t.seconds);
-
-      if (t.totel <= 0) {
-        clearInterval(timeInterval);
-      }
-    }
-  }
-
-  setClock('.timer', deadline);
-
-  // MODAL My Version
-
-  // const modalOpen = document.querySelectorAll('[data-modal]'),
-  //       modalClose = document.querySelector('[data-modal-close]'),
-  //       modal = document.querySelector('.modal');
-  
-  // modalOpen.forEach(item => {
-  //   item.addEventListener('click', () => {
-  //     modal.style.display = 'block';
-  //   })
-  // });
-
-  // modalClose.addEventListener('click', () => {
-  //   modal.style.display = 'none';
-  // })
-
-  // End MODAL My Version
-
-  // Modal Teacher Versin
-
-  const modalTrigger = document.querySelectorAll('[data-modal]'),
-        modal = document.querySelector('.modal'),
-        modalCloseBtn = document.querySelector('[data-modal-close]');
-
-  function openModal() {
-    modal.classList.add('show');
-    modal.classList.remove('hide');
-    document.body.style.overflow = 'hidden';
-
-    clearInterval(modalTimerId);
-  }
-
-  modalTrigger.forEach(btn => {
-    btn.addEventListener('click', openModal)
-  });
-
-  
-
-  function closeModal() {
-    modal.classList.add('hide');
-    modal.classList.remove('show');
-    document.body.style.overflow = '';
-  }
-
-  modalCloseBtn.addEventListener('click', closeModal);
-
-  modal.addEventListener('click', (e) => {
-    if (e.target === modal) {
-      closeModal();
-    }
-  });
-
-  document.addEventListener('keydown', (e) => {
-    if (e.code === 'Escape' && modal.classList.contains('show')) {
-      console.log('escape');
-      closeModal();
-    }
-  })
-
-  // End Modal Teacher Versin
-
-  const modalTimerId = setTimeout(openModal, 5000);
-
-  function showModalByScroll() {
-    if (window.pageYOffset + document.documentElement.clientHeight >= document.documentElement.scrollHeight) {
-      openModal();
-      window.removeEventListener('scroll', showModalByScroll);
-    }
-  }
-
-  window.addEventListener('scroll', showModalByScroll);
-});
+console.log(ivan);
+console.log(arauz);
