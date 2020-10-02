c3d158a5987e5c157678df08c83bbd15c9cc820f
Helicatic <arauzworker@gmail.com>
2020-10-02 16:01:07 +0500

    Урок 47 Классы (ES6)

--- js/script.js
+++ js/script.js
@@ -1,80 +1,35 @@
 "use strict";
 
-function showThis(a, b) {
-  console.log(this);
-  function sum() {
-    console.log(this);
-    return a + b;
+class Rectangle {
+  constructor(height, width) {
+    this.height = height;
+    this.width = width;
   }
-  console.log(sum());
-}
 
-// showThis(4, 5);
-
-const obj = {
-  a: 20,
-  b:15,
-  sum: function() {
-    function shout() {
-      console.log(this);
-    }
-    shout();
+  calcArea() {
+    return this.height * this.width;
   }
 }
 
-// obj.sum();
-
-// function User(name, id) {
-//   this.name = name;
-//   this.id = id;
-//   this.human = true;
-// }
-// let arauz = new User('Arauz', 18);
-
-// function sayName(surname) {
-//   console.log(this);
-//   console.log(this.name + ' ' + surname);
-// }
-// const user = {
-//   name: 'John'
-// }
-
-// sayName.call(user, 'Johnes');
-// sayName.apply(user, ['Jonathan']);
-
-// function count(num) {
-//   return this*num;
-// }
-
-// const double = count.bind(2);
-// console.log(double(3));
-// console.log(double(15));
-
-
-// 1) Обычная функция this будет равна window, но если будет стоять use strict то будет undefined
-// 2) Контекст у методов объекта будет сам объект
-// 3) this в конструкторах и классах это новый экземпляр объекта
-// 4) Ручная привязка this: call, apply, bind
-
-const btn = document.querySelector('button');
-
-btn.addEventListener('click', function() {
-  // console.log(this);
-  this.style.backgroundColor = 'red';
-})
-
-const objects = {
-  num: 5,
-  sayNumber: function() {
-    const say = () => {
-      console.log(this.num);
-    };
-    say();
+class ColoredRectangleWithText extends Rectangle {
+  constructor(height, width, text, bgColor) {
+    super(height, width);
+    this.text = text;
+    this.bgColor = bgColor;
   }
-};
 
-objects.sayNumber();
+  showMyProps() {
+    console.log(`Текст: ${this.text}, цвет: ${this.bgColor}`);
+  }
+}
 
-const doubled = a => a * 2;
+const div = new ColoredRectangleWithText(25, 10, 'Hello world', 'red')
 
-console.log(doubled(5));
+div.showMyProps();
+console.log(div.calcArea());
+
+// const square = new Rectangle(10, 12);
+// const long = new Rectangle(20, 100);
+
+// console.log(square.calcArea());
+// console.log(long.calcArea());
