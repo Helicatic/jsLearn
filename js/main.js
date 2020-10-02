a84a90771d5641469cd3aa9676790c1835c82cc3
Helicatic <arauzworker@gmail.com>
2020-10-02 18:48:07 +0500

    Урок 49 Rest оператор и параметры по умолчанию (ES6)

---
+++ js/main.js
@@ -0,0 +1,11 @@
+// const log = function(a, b, ...rest) {
+//   console.log(a, b, rest);
+// }
+
+// log('basic', 'rest', 'operator', 'usage', 'asshole');
+
+// function calcOrDouble(number, basis = 2) {
+//   // basis = basis || 2; Старый способ задать значение по умолчанию
+//   console.log(number * basis);
+// }
+// calcOrDouble(3);
