2771fbb0506b289c0d4b46fc39afe9a953d82cd9
Helicatic <helicatic@users.noreply.github.com>
2020-10-14 15:21:32 +0500

    Урок 57 Методы перебора массивов

--- backups/8, 9 уроки/main.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-
-const category = "toys";
-
-console.log(`https://github.com/${category}/5`);
-
-const username = "Rustam";
-
-alert(`Hello, ${username}`);
--- js/script.js
+++ js/script.js
@@ -1,307 +1,73 @@
 "use strict";
-document.addEventListener("DOMContentLoaded", () => {
-  // TABS
 
-  const tabs = document.querySelectorAll(".tabheader__item"),
-    tabsContent = document.querySelectorAll(".tabcontent"),
-    tabsParent = document.querySelector(".tabheader__items");
+// FILTER 
 
-  function hideTabContent() {
-    tabsContent.forEach((item) => {
-      item.classList.add("hide");
-      item.classList.remove("show", "fade");
-    });
+const names = ['Rustam', 'Alex', 'Vorzamat', 'Jumanazar', 'Mo'];
 
-    tabs.forEach((item) => {
-      item.classList.remove("tabheader__item_active");
-    });
-  }
-
-  function showTabContent(i = 0) {
-    tabsContent[i].classList.add("show", "fade");
-    tabsContent[i].classList.remove("hide");
-    tabs[i].classList.add("tabheader__item_active");
-  }
-
-  hideTabContent();
-  showTabContent();
-
-  tabsParent.addEventListener("click", (e) => {
-    const target = e.target;
-
-    if (target && target.classList.contains("tabheader__item")) {
-      tabs.forEach((item, i) => {
-        if (target == item) {
-          hideTabContent();
-          showTabContent(i);
-        }
-      });
-    }
-  });
-
-  // TIMER
-
-  const deadline = "2020-10-12";
-
-  function getTimeRemaining(endtime) {
-    const t = Date.parse(endtime) - Date.parse(new Date()),
-      days = Math.floor(t / (1000 * 60 * 60 * 24)),
-      hours = Math.floor((t / (1000 * 60 * 60)) % 24),
-      minutes = Math.floor((t / 1000 / 60) % 60),
-      seconds = Math.floor((t / 1000) % 60);
-
-    return {
-      total: t,
-      days: days,
-      hours: hours,
-      minutes: minutes,
-      seconds: seconds,
-    };
-  }
-
-  function getZero(num) {
-    if (num >= 0 && num < 10) {
-      return `0${num}`;
-    } else {
-      return num;
-    }
-  }
-
-  function setClock(selector, endtime) {
-    const timer = document.querySelector(selector),
-      days = timer.querySelector("#days"),
-      hours = timer.querySelector("#hours"),
-      minutes = timer.querySelector("#minutes"),
-      seconds = timer.querySelector("#seconds"),
-      timeInterval = setInterval(updateClock, 1000);
-
-    updateClock();
-
-    function updateClock() {
-      const t = getTimeRemaining(endtime);
-
-      days.innerHTML = getZero(t.days);
-      hours.innerHTML = getZero(t.hours);
-      minutes.innerHTML = getZero(t.minutes);
-      seconds.innerHTML = getZero(t.seconds);
-
-      if (t.totel <= 0) {
-        clearInterval(timeInterval);
-      }
-    }
-  }
-
-  setClock(".timer", deadline);
-
-  const modalTrigger = document.querySelectorAll("[data-modal]"),
-    modal = document.querySelector(".modal");
-
-  function openModal() {
-    modal.classList.add("show");
-    modal.classList.remove("hide");
-    document.body.style.overflow = "hidden";
-
-    clearInterval(modalTimerId);
-  }
-
-  modalTrigger.forEach((btn) => {
-    btn.addEventListener("click", openModal);
-  });
-
-  function closeModal() {
-    modal.classList.add("hide");
-    modal.classList.remove("show");
-    document.body.style.overflow = "";
-  }
-
-
-  modal.addEventListener("click", (e) => {
-    if (e.target === modal || e.target.getAttribute('data-close') == '') {
-      closeModal();
-    }
-  });
-
-  document.addEventListener("keydown", (e) => {
-    if (e.code === "Escape" && modal.classList.contains("show")) {
-      console.log("escape");
-      closeModal();
-    }
-  });
-
-  // End Modal Teacher Versin
-
-  const modalTimerId = setTimeout(openModal, 50000);
-
-  function showModalByScroll() {
-    if (
-      window.pageYOffset + document.documentElement.clientHeight >=
-      document.documentElement.scrollHeight
-    ) {
-      openModal();
-      window.removeEventListener("scroll", showModalByScroll);
-    }
-  }
-
-  window.addEventListener("scroll", showModalByScroll);
-
-  // Используем классы для карточек
-
-  class MenuCard {
-    constructor(src, alt, title, descr, price, parentSelector, ...classes) {
-      this.src = src;
-      this.alt = alt;
-      this.title = title;
-      this.descr = descr;
-      this.price = price;
-      this.classes = classes;
-      this.parent = document.querySelector(parentSelector);
-      this.transfer = 27;
-      this.chengeToUAH();
-    }
-
-    chengeToUAH() {
-      this.price = this.price * this.transfer;
-    }
-    render() {
-      const element = document.createElement("div");
-      if (this.classes.length === 0) {
-        this.element = "menu__item";
-        element.classList.add(this.element);
-      } else {
-        this.classes.forEach((className) => element.classList.add(className));
-      }
-      element.innerHTML = `
-          <img src=${this.src} alt=${this.alt}>
-          <h3 class="menu__item-subtitle">${this.title}</h3>
-          <div class="menu__item-descr">${this.descr}</div>
-          <div class="menu__item-divider"></div>
-          <div class="menu__item-price">
-            <div class="menu__item-cost">Цена:</div>
-            <div class="menu__item-total"><span>${this.price}</span> грн/день</div>
-          </div>
-      `;
-      this.parent.append(element);
-    }
-  }
-
-  new MenuCard(
-    "img/tabs/vegy.jpg",
-    "vegy",
-    'Меню "Фитнес"',
-    'Меню "Фитнес" - это новый подход к приготовлению блюд: больше свежих овощей и фруктов. Продукт активных и здоровых людей. Это абсолютно новый продукт с оптимальной ценой и высоким качеством!',
-    9,
-    ".menu .container"
-  ).render();
-
-  new MenuCard(
-    "img/tabs/elite.jpg",
-    "elite",
-    "Меню “Премиум”",
-    "В меню “Премиум” мы используем не только красивый дизайн упаковки, но и качественное исполнение блюд. Красная рыба, морепродукты, фрукты - ресторанное меню без похода в ресторан!",
-    14,
-    ".menu .container",
-    "menu__item"
-  ).render();
-
-  new MenuCard(
-    "img/tabs/post.jpg",
-    "post",
-    'Меню "Постное"',
-    "Меню “Постное” - это тщательный подбор ингредиентов: полное отсутствие продуктов животного происхождения, молоко из миндаля, овса, кокоса или гречки, правильное количество белков за счет тофу и импортных вегетарианских стейков.",
-    21,
-    ".menu .container",
-    "menu__item"
-  ).render();
-
-  // FORMS
-
-  const forms = document.querySelectorAll("form");
-
-  const message = {
-    loading: "img/form/spinner.svg",
-    success: "Спасибо! Скоро мы с вами свяжемся",
-    failure: "Что-то пошло не так...(",
-  };
-
-  forms.forEach((item) => {
-    postData(item);
-  });
-
-  function postData(form) {
-    form.addEventListener("submit", (e) => {
-      e.preventDefault();
-
-      const statusMessage = document.createElement("img");
-      statusMessage.src = message.loading;
-      statusMessage.style.cssText = `
-        display: block;
-        margin: 0 auto;
-      `
-      // form.append(statusMessage);
-      form.insertAdjacentElement('afterend', statusMessage);
-
-      
-      const formData = new FormData(form);
-
-      const object = {};
-      formData.forEach(function (value, key) {
-        object[key] = value;
-      });
-
-      fetch('server.php', {
-        method: "POST",
-        headers: {
-          'Content-type': 'application/json'
-        },
-        body: JSON.stringify(object)
-      })
-      .then(data => data.text())
-      .then(data => {
-        console.log(data);
-        showThanksModal(message.success);
-        statusMessage.remove();
-      })
-      .catch(() => {
-        showThanksModal(message.failure);
-      })
-      .finally(() => {
-        form.reset();
-      })
-    });
-  }
-
-  function showThanksModal(message) {
-    const previusModalDialog = document.querySelector('.modal__dialog');
-
-    previusModalDialog.classList.add('hide');
-    openModal();
-
-    const thanksModal = document.createElement('div');
-    thanksModal.classList.add('modal__dialog');
-    thanksModal.innerHTML = `
-      <div class="modal__content">
-        <div class="modal__close" data-close>×</div>
-        <div class="modal__title">${message}</div>
-      </div>
-    `;
-
-    document.querySelector('.modal').append(thanksModal);
-
-    setTimeout(() => {
-      thanksModal.remove();
-      previusModalDialog.classList.add('show');
-      previusModalDialog.classList.remove('hide');
-      closeModal();
-    }, 4000)
-  }
-
-  fetch('https://jsonplaceholder.typicode.com/posts', {
-    method: "POST",
-    body: JSON.stringify({name: 'Alex'}),
-    headers: {
-      'Content-type': 'applycation/json'
-    }
-  })
-    .then(response => response.json())
-    .then(json => console.log(json));
+const shortNames = names.filter(name => {
+  return name.length < 5;
 });
+
+console.log(shortNames);
+
+
+// MAP
+
+
+const answers = ['RUStaM', 'MayMUn', 'Hello'];
+const result = answers.map(item => item.toLowerCase());
+console.log(result);
+
+let answers = ['RUStaM', 'MayMUn', 'Hello'];
+answers = answers.map(item => item.toLowerCase());
+console.log(answers);
+
+
+// EVERY/SOME
+
+
+const some = [4, 'qwr', 'whgsha'];
+
+console.log(some.some(item => typeof(item) === 'number'));
+
+
+const some = [4, 4, 2];
+
+console.log(some.every(item => typeof(item) === 'number'));
+
+
+// REDUCE
+
+
+const arr = [4, 1, 5, 3, 9, 7];
+                          // 0     4
+                          // 4     1
+                          // 5     5
+                          // 10    3
+                          // 13    9
+                          // 22    7
+                          // 29
+const res = arr.reduce((sum, current) => sum + current);
+
+console.log(res);
+
+
+const arr = ['apple', 'strawberry', 'melon'];
+
+const res = arr.reduce((sum, current) => `${sum}, ${current}`);
+
+console.log(res);
+
+
+const obj = {
+  rustam: 'persone',
+  ann: 'persone',
+  dog: 'animal',
+  cat: 'animal',
+};
+
+const newArr = Object.entries(obj)
+.filter(item => item[1] === 'persone')
+.map(item => item[0]);
+
+console.log(newArr);
