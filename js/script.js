192e7428528ec6681fd0686db27953d72c5ddb39
Helicatic <helicatic@users.noreply.github.com>
2020-10-28 19:27:35 +0500

    Урок 61 Создаем слайдер на сайте, вариант 1

--- js/script.js
+++ js/script.js
@@ -299,4 +299,59 @@ document.addEventListener("DOMContentLoaded", () => {
   fetch('http://localhost:3000/menu')
     .then(data => data.json())
     .then(res => console.log(res));
+
+
+    // SLIDER 
+
+    const slides = document.querySelectorAll('.offer__slide'),
+    slidePrev = document.querySelector('.offer__slider-prev'),
+    slideNext = document.querySelector('.offer__slider-next'),
+    sliderCounter = document.querySelector('.offer__slider-counter'),
+    currentSlide = sliderCounter.querySelector('#current'),
+    totalSlides = sliderCounter.querySelector('#total');
+    let slideIndex = 1;
+
+    showSlides(slideIndex);
+
+    if (slides.length < 10) {
+      totalSlides.textContent = `0${slides.length}`;
+    } else {
+      totalSlides.textContent = slides.length;
+    }
+
+    function showSlides(n) {
+      if (n > slides.length) {
+      slideIndex = 1;
+      }
+
+      if (n < 1) {
+      slideIndex = slides.length;
+      }
+
+      slides.forEach(slide => slide.style.display = 'none');
+
+      slides[slideIndex - 1].style.display = 'block';
+
+      if (slides.length < 10) {
+        currentSlide.textContent = `0${slideIndex}`;
+      } else {
+        currentSlide.textContent = slideIndex;
+      }
+    }
+
+    function plusSlides(n) {
+      showSlides(slideIndex += n);
+    }
+
+    slidePrev.addEventListener('click', () => {
+    plusSlides(-1);
+    });
+    
+    slideNext.addEventListener('click', () => {
+      plusSlides(1);
+    });
+
+    
 });
+
+
