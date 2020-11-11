e61184afeeb3643bd2103bb99597c6818db86801
Helicatic <helicatic@users.noreply.github.com>
2020-11-11 23:04:19 +0500

    Урок 69 Инкапсуляция

--- js/lesson.js
+++ js/lesson.js
@@ -1,17 +1,67 @@
 'use strict';
 
-const persone = {
-  name: 'Alex',
-  age: 27,
+// function User(name, age) {
+//   this.name = name;
+//   let userAge = age;
 
-  get userAge() {
-    return this.age;
-  },
+//   this.say = function() {
+//     console.log(`Имя пользователя: ${this.name}, возраст: ${userAge}`);
+//   };
 
-  set userAge(num) {
-    this.age = num;
+//   this.getAge = function() {
+//     return userAge;
+//   };
+
+//   this.setAge = function(age) {
+//     if (typeof age === 'number' && age > 0 && age < 110) {
+//       userAge = age;
+//     } else {
+//       console.log('Недопустимое значение');
+//     }
+//   };
+// }
+
+class User {
+  constructor(name, age) {
+    this.name = name;
+    this._age = age;
   }
-};
 
-console.log(persone.userAge = 30);
-console.log(persone.userAge);
+  #surname = 'Azimov'; // Приватное свойство
+
+  say = () => {
+    console.log(`Имя пользователя: ${this.name} ${this.#surname}, возраст: ${this._age}`);
+  }
+
+  get age() {
+    return this._age;
+  }
+
+  set age(age) {
+    if (typeof age === 'number' && age > 0 && age < 110) {
+      this._age = age;
+    } else {
+      console.log('Недопустимое значение');
+    }
+  }
+}
+
+
+const ivan = new User('Ivan', 27);
+console.log(ivan.surname);
+// console.log(ivan._age);
+// ivan._age = 99;
+// console.log(ivan._age);
+
+// console.log(ivan.userAge);
+// console.log(ivan.getAge());
+
+// ivan.setAge(30);
+// ivan.setAge(300);
+// console.log(ivan.getAge());
+
+
+// ivan.userAge = 30;
+// ivan.name = 'Alex';
+
+ivan.say();
