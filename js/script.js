781d55412b17153e5883ab672bf17e372235caed
Helicatic <helicatic@users.noreply.github.com>
2020-11-13 14:58:55 +0500

    Урок 71 Webpack. Собираем наш проект

--- js/script.js
+++ js/script.js
@@ -1,25 +1,18 @@
-'use strict';
+"use strict";
 
-const app = '21';
-
-const num = 1;
-
-(function(){
-  let num = 2;
-  console.log(num);
-  console.log(num + 3);
-}());
-
-console.log(num);
-
-const user = (function(){
-  const privat = function() {
-    console.log("Private func");
-  };
-
-  return {
-    sayHello: privat
-  };
-}());
-
-user.sayHello();
+document.addEventListener("DOMContentLoaded", () => {
+    const tabs = require('./modules/tabs'),
+          modal = require('./modules/modal'),
+          timer = require('./modules/timer'),
+          cards = require('./modules/cards'),
+          forms = require('./modules/forms'),
+          slider = require('./modules/slider'),
+          calc = require('./modules/calc');
+    tabs();
+    modal();
+    timer();
+    cards();
+    forms();
+    slider();
+    calc();
+});
